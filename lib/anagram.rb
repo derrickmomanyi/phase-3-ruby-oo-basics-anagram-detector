7960c2424a4dca84195ffe964bcd5402bd5d213b
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-02-09 18:50:55 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram 
+    attr_reader :word 
+    
+    def initialize(word) 
+        @word = word 
+    end 
+    def match(array) 
+        array.filter { |letter|   letter.split("").sort == word.split("").sort }
+     end 
+    end 
+    
+    
+jina = Anagram.new("listen")
